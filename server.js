516a7e0745e9e9d12b829386c52aa900928bd054
Kordan <kordan@hitripod.com>
2013-11-10 23:27:13 +0800

    Resolved.

--- server.js
+++ server.js
@@ -41,47 +41,6 @@ app.post('/down', function(req, res) {
     down.convert(req, res);
 });
 
-/*
-app.post('/file-upload', function(req, res, next) {
-    console.log(req.body);
-    console.log(req.files);
-});
-*/
-
-///*
-// we need the fs module for moving the uploaded files
-//var fs = require('fs');
-//app.post('/file-upload', function(req, res) {
-/*
-    console.log(req.body);
-    console.log(req.files);
-    // get the temporary location of the file
-    var tmp_path = req.files.thumbnail.path;
-    console.log(tmp_path);
-    // set where the file should actually exists - in this case it is in the "images" directory
-    var target_path = './public/images/' + req.files.thumbnail.name;
-    // move the file from the temporary location to the intended location
-    fs.rename(tmp_path, target_path, function(err) {
-        if (err) throw err;
-        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-        fs.unlink(tmp_path, function() {
-            if (err) throw err;
-            res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
-        });
-    });
-*/
-//});
-//*/
-
-///*
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on  port " + app.get('port'));
-});
-//*/
-
-
-//everyauth.helpExpress(app);
-/*
 var options = {
     key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
     cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem'))
@@ -90,6 +49,3 @@ var options = {
 https.createServer(options, app).listen('443', function(){
     console.log("Express server listening on port " + app.get('port'));
 });
-//http.createServer(app).listen(app.get('port'), function(){
-//  console.log("Express server listening on port " + app.get('port'));
-//});
