364c7e4480b4b9bd413647c71275fd46fc72c1d7
Don <don@don.com>
2013-11-10 20:07:48 +0800

    Disable passport

--- server.js
+++ server.js
@@ -10,17 +10,30 @@ var express = require('express')
   , http = require('http')
   , https = require('https')
   , fs = require('fs')
-  , path = require('path')
-  , passport = require('passport')
-  , util = require('util')
-  , DropboxStrategy = require('passport-dropbox').Strategy;
+  , path = require('path');
+//  , conf = require('./conf')
+//  , everyauth = require('everyauth');
+//  , passport = require('passport')
+//  , util = require('util')
+//  , DropboxStrategy = require('passport-dropbox').Strategy;
 
 //var DROPBOX_APP_KEY = "--insert-dropbox-app-key-here--"
 //var DROPBOX_APP_SECRET = "--insert-dropbox-app-secret-here--";
+///*
+//var DROPBOX_APP_KEY = "8927n1gidx5s9kr"
+//var DROPBOX_APP_SECRET = "mm7vqtligthgh0n";
 
-var DROPBOX_APP_KEY = "8927n1gidx5s9kr"
-var DROPBOX_APP_SECRET = "mm7vqtligthgh0n";
+/*
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username, password: password }, function (err, user) {
+      done(err, user);
+    });
+  }
+));
+*/
 
+/*
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -28,22 +41,11 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
-/*
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-*/
 passport.use(new DropboxStrategy({
     consumerKey: DROPBOX_APP_KEY,
     consumerSecret: DROPBOX_APP_SECRET,
     //callbackURL: "http://127.0.0.1:3000/auth/dropbox/callback"
-    callbackURL: "http://127.0.0.1:80/auth/dropbox/callback"
+    callbackURL: "http://localhost//auth/dropbox/callback"
   },
   function(token, tokenSecret, profile, done) {
     // asynchronous verification, for effect...
@@ -57,12 +59,15 @@ passport.use(new DropboxStrategy({
     });
   }
 ));
-
+*/
+//*/
 
   //, conf = require('./conf')
   //, everyauth = require('everyauth');
+///*
 /*
 everyauth.debug = true;
+
 var usersById = {};
 everyauth.everymodule
   .findUserById( function (id, callback) {
@@ -84,6 +89,12 @@ function addUser (source, sourceUser) {
 }
 
 var usersByFbId = {};
+var usersByDropboxId = {};
+
+everyauth.dropbox
+  .entryPath('/auth/dropbox')
+  .callbackPath('/auth/dropbox/callback');
+
 everyauth
   .facebook
     .appId(conf.fb.appId)
@@ -93,39 +104,59 @@ everyauth
         (usersByFbId[fbUserMetadata.id] = addUser('facebook', fbUserMetadata));
     })
     .redirectPath('/');
-
+//*/
+/*
 everyauth
   .dropbox
     .consumerKey(conf.dropbox.consumerKey)
     .consumerSecret(conf.dropbox.consumerSecret)
+    //.entryPath('/auth/dropbox')
+    //.callbackPath('/auth/dropbox/callback')
     .findOrCreateUser( function (sess, accessToken, accessSecret, dropboxUserMetadata) {
+      console.log(sess);
+      console.log(accessToken);
+      console.log(accessSecret);
+      console.log(dropboxUserMetadata);
       return usersByDropboxId[dropboxUserMetadata.uid] ||
         (usersByDropboxId[dropboxUserMetadata.uid] = addUser('dropbox', dropboxUserMetadata));
     })
     .redirectPath('/');
 */
+//*/
+/*
+everyauth.dropbox
+  .entryPath('/auth/dropbox')
+  .callbackPath('/auth/dropbox/callback');
+*/
+//everyauth.dropbox.configurable();
+
+
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 80);
+  //app.set('port', process.env.PORT || 443);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.bodyParser());
+  //app.use(express.bodyParser());
   app.use(express.methodOverride());
-  //app.use(app.router);
+  app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 
+  app.use(express.bodyParser({uploadDir:'./public/uploads'}));
   //app.use(express.bodyParser());
   //app.use(express.cookieParser('mr ripley'));
-  app.use(express.cookieParser('downAndUp'));
-  //app.use(express.session());
-  app.use(express.session({ secret: 'keyboard cat' }));
+  //app.use(express.cookieParser('downAndUp'));
+  //app.use(express.static(__dirname + '/../public'));
+  app.use(express.session());
+  //app.use(everyauth.middleware(app));
+  //app.use(express.session({ secret: 'keyboard cat' }));
   //app.use(express.session({ secret: 'skwp$^f9fw32[p-' }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
+  //app.use(passport.initialize());
+  //app.use(passport.session());
+  //app.use(app.router);
   //app.use(everyauth.middleware());
 });
 
@@ -147,6 +178,7 @@ app.get('/', function(req, res){
   res.render(routes.index, { user: req.user });
 });
 */
+/*
 app.get('/account', ensureAuthenticated, function(req, res){
   res.render('account', { user: req.user });
 });
@@ -154,6 +186,12 @@ app.get('/account', ensureAuthenticated, function(req, res){
 app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
+
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 app.get('/auth/dropbox',
   passport.authenticate('dropbox'),
@@ -172,13 +210,7 @@ app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
-
-
-
-
-
-
-
+*/
 
 
 /*
@@ -193,32 +225,69 @@ app.get('/', function (req, res) {
 });
 */
 app.get('/editor', down.index);
-app.get('/down', down.convert);
-app.get('/login', down.login);
+//app.get('/down', down.convert);
+//app.get('/login', down.login);
 
 //app.use(express.bodyParser());
 app.post('/down', function(req, res) {
     down.convert(req, res);
 });
 
-
 /*
-var options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
+app.post('/file-upload', function(req, res, next) {
+    console.log(req.body);
+    console.log(req.files);
+});
 */
+
+///*
+// we need the fs module for moving the uploaded files
+var fs = require('fs');
+app.post('/file-upload', function(req, res) {
+/*
+    console.log(req.body);
+    console.log(req.files);
+    // get the temporary location of the file
+    var tmp_path = req.files.thumbnail.path;
+    console.log(tmp_path);
+    // set where the file should actually exists - in this case it is in the "images" directory
+    var target_path = './public/images/' + req.files.thumbnail.name;
+    // move the file from the temporary location to the intended location
+    fs.rename(tmp_path, target_path, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+        fs.unlink(tmp_path, function() {
+            if (err) throw err;
+            res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
+        });
+    });
+*/
+});
+//*/
+
 ///*
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on  port " + app.get('port'));
 });
 //*/
-//https.createServer(options, app).listen(app.get('port'), function(){
-//  console.log("Express server listening on port " + app.get('port'));
-//});
+
+
+//everyauth.helpExpress(app);
+/*
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
 //*/
 
+
+/*
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login')
 }
+*/
